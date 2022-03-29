32e7b9a6f6201699851f047ae55420a9ded2f837
Brent Clements <brent.clements@gmail.com>
2022-03-29 02:05:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-setup(name='unifipy-api-client',
+setup(name='unifipy_api_client',
       version='1.0',
       description='Client SDK for Ubiquity Networks UniFi controller',
       author='Brent Clements',
       author_email='brent.clements@gmail.com',
       url='https://github.com/bclements/unifipy-api-client',
-      packages=['unifipy-api-client'],      
+      packages=['unifipy_api_client'],      
       classifiers=[],
       install_requires=['requests'],
       )
